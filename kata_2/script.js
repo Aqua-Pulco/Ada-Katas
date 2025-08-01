2d9161d9b4c9face9d43f43932510f855d04952b
AquaPulco <iris.yolal@gmail.com>
2025-08-01 17:19:19 +0200

    error h1

--- kata_2/script.js
+++ kata_2/script.js
@@ -1,4 +1,4 @@
- //https://domaine.tld/chemin/endpoint?param1=valeur1&param2=valeur2
+//https://domaine.tld/chemin/endpoint?param1=valeur1&param2=valeur2
 //domaine1 = nominatim.openstreetmap.org
 //domaien2 = api.open-meteo.com
 //chemin/endpoint1 = /search
@@ -7,43 +7,66 @@
 //?param=value2 = ?latitude=${float1}&longitude=${float2}&...en temps reel =true hourly = par heure je veux : humidity temperature precipations etc
 //puis on a les formats, limits etc
 
+//function.then(var => action(var)) .then
+
+
 let LAT, LON;
-let LAT_BRUT,LON_BRUT;
 
 
 const getWeather = async (latitude, longitude) => {
-    try{
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,precipitation&timezone=auto`);
-    const weatherData = await response.json();
-    console.log(Object.keys(weatherData));
-    return weatherData["current_weather"].temperature;
-   
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,precipitation&timezone=auto`);
+        const weatherData = await response.json();
+        // console.log(Object.keys(weatherData));
+        console.log(weatherData["current_weather"].temperature);
+        return weatherData["current_weather"].temperature;
+
     }
-    catch{
+    catch {
 
     }
 
 }
 
 //
-const getLocation = async  (location) => {
-    try{
-    const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${location}&format=json&addressdetails=1&limit=1`);
-    const data = await response.json();
-    console.log(data);
-    LAT_BRUT = Number(data[0].lat);
-    LON_BRUT = Number(data[0].lon);
-    console.log(await getWeather(LAT_BRUT, LON_BRUT));
-    
-    }
-    catch{
+const getCoord = async (location) => {
+    try {
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${location}&format=json&addressdetails=1&limit=1`);
+        const data = await response.json();
 
+        if (data.length === 0) {
+            throw new Error("Ville non trouvée");
+        }
+    
+        LAT = Number(data[0].lat);
+        LON = Number(data[0].lon);
+
+        return await LAT, await LON;
+    }
+    catch (error) {
+        return h1.innerText= error.message;
     }
 }
-getLocation('lyon')
 
 
 
+    
+const userInput = document.querySelector("#cityInput");
+const okBtn = document.querySelector("#submitBtn");
+const h1 = document.querySelector("#city"); 
+const temperature = document.querySelector("#temperature"); 
+const details = document.querySelector("#details"); 
+const gpsInfos = document.querySelector("#gps"); 
+
+
+    okBtn.addEventListener("click", (e) =>{
+        let temp;
+        const ville = userInput.value
+        h1.innerHTML = ville;
+        getCoord(ville).then(response =>  temp = return);
+        
+
+    })
 
 
 
