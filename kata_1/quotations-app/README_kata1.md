9189bfb783fc9250f76e8795d2afcfddae103488
AquaPulco <iris.yolal@gmail.com>
2025-08-01 10:08:15 +0200

    kata_2 v0

--- kata_1/quotations-app/README_kata1.md
+++ kata_1/quotations-app/README_kata1.md
@@ -1,3 +1,27 @@
 README DU KATA 1 04/07/2025
 
 depot git fonctions et appel de fonctions
+
+form.addEventListener("submit", function(event){ //(let event = submit)
+    event.preventDefault();
+
+    const quote = document.getElementById("quote-text").value.trim();
+    const author = document.getElementById("quote-author").value.trim();
+
+    console.log("quote is", quote)
+    console.log("author is", author)
+})
+
+<!-- if(quote && author){
+    addQuote(quote, author)
+} -->
+
+qd on ecout eun form event = "submit"
+
+const quote = document.getElementById("quote-text").value.trim()
+
+.trim() => enleve les espace blancs avant et apres
+
+
+
+qd on clique sur submit le comportement par defaut et de rafraichir la page
--- kata_1/quotations-app/script.js
+++ kata_1/quotations-app/script.js
@@ -11,23 +11,23 @@ let div = document.getElementById("quote-list");
 let madata = localStorage.getItem("citations");
 console.log(madata); //leeet's see 🔎
 
-let tab = []; 
+let tab = [];
 
 //localStorage.clear(); // 🙏
 
 // il y a deux possibilités soit le localstorage est null
-    //console.log(madata); > null
-    //il n'a pas été investi
-    // a remplir
+//console.log(madata); > null
+//il n'a pas été investi
+// a remplir
 
 // soit il est deja rempli
-    //console.log(madata); 
-    //[{"auteur":"untel1","citation":"mots1","numero":1},{"auteur":"untel2","citation":"mots2","numero":2}]
-    //tableau d'objets mais strings (et oui c'est du JSON !!!)
-    // IL FAUT LES RETRANSFORMER EN objet tableau (parse)
+//console.log(madata); 
+//[{"auteur":"untel1","citation":"mots1","numero":1},{"auteur":"untel2","citation":"mots2","numero":2}]
+//tableau d'objets mais strings (et oui c'est du JSON !!!)
+// IL FAUT LES RETRANSFORMER EN objet tableau (parse)
 
 // je verifie si le tiroir existe
-if (madata === null){ //si il n'existe pas tab reste vide
+if (madata === null) { //si il n'existe pas tab reste vide
     tab = [];
 }
 else {
@@ -38,51 +38,58 @@ console.log("a quoi ressemble mon tableau", tab)
 let compteur = tab.length;
 h3Text();
 
-// crée une balise en dynamique html a partir d'une id existante
-function nouvelleBalise(typeBalise, texte, id){
+
+// info  input=date <inputid= "inputAge" type="date" />
+/**
+ * crée une balise en dynamique html a partir d'une id existante
+ * @param {"balise"} typeBalise 
+ * @param {"text"} texte 
+ * @param {"ref"} id 
+ * @returns 
+ */
+
+function nouvelleBalise(typeBalise, texte, id) {
     let element = document.createElement(`${typeBalise}`);
     element.innerText = texte;
-    return id.appendChild(element);
+    return id.appendChild(element); //demander a Jeremy pour autre que id
 }
 
 function recupInput(id1) {
-   const txt = document.getElementById(`${id1}`).value;
-   if (txt === ""){ // si rien, renvoie null > pour pouvoir sortir du click
-    return null
-   }
-   else {
+    const txt = document.getElementById(`${id1}`).value;
+    if (txt === "") { // si rien, renvoie null > pour pouvoir sortir du click
+        return null
+    }
+    else {
         return txt
-   }
+    }
 }
 
-function effaceChampInput(id1){
+function effaceChampInput(id1) {
     return document.getElementById(`${id1}`).value = "";
 }
 
-function addQuote(text, author){ // affiche dynamiquement ds le html 
+function addQuote(text, author) { // affiche dynamiquement ds le html 
     const p1 = nouvelleBalise("p", text, div);
     const p2 = nouvelleBalise("B", author, div);
     p1.className = "quote";
     p2.className = "author";
-    
+
     return p1, p2
 }
 
-function h3Text(){
-       // texte du compteur
-    if (compteur <= 1)
-            {
-                return h3.innerText = `${compteur} citation`
-            }
-        else
-            {
-                return h3.innerText = `${compteur} citations`
-            }
+function h3Text() {
+    // texte du compteur
+    if (compteur <= 1) {
+        return h3.innerText = `${compteur} citation`
+    }
+    else {
+        return h3.innerText = `${compteur} citations`
+    }
 }
 
- for(let item of tab){
-        addQuote(item.citation, item.auteur)
-    }  
+for (let item of tab) {
+    addQuote(item.citation, item.auteur)
+}
 
 
 submit.addEventListener("click", () => {
@@ -91,11 +98,11 @@ submit.addEventListener("click", () => {
 
     //si pas de caractère dans les inputs > récupère le 'null' du return recupInput
     if (input1 === null || input2 === null) {
-    // on ne compte rien, on sort du click, on ne va pas plus loin.
-            return;
-        }
-    else{
-    //on compte et on affiche h3
+        // on ne compte rien, on sort du click, on ne va pas plus loin.
+        return;
+    }
+    else {
+        //on compte et on affiche h3
         compteur++
         h3Text();
     }
@@ -103,11 +110,11 @@ submit.addEventListener("click", () => {
     addQuote(input1);
 
     //on remplit notre tableau
-    tab.push({auteur: input2, citation: input1, numero: compteur});
+    tab.push({ auteur: input2, citation: input1, numero: compteur });
     //console LOGGGG
-    console.log(tab); 
+    console.log(tab);
 
-  
+
     //pour envoyer tab dans le JSON je dois le transformer en strings
     let tab2 = JSON.stringify(tab);
 
@@ -115,16 +122,16 @@ submit.addEventListener("click", () => {
 
     //console LOGGGG
     console.log(localStorage.getItem("citations"));
-            //console LOGGGG
-            console.log(localStorage);
-     
-      
-        effaceChampInput("citation");
-        effaceChampInput("nomAuteur");  
-    })
+    //console LOGGGG
+    console.log(localStorage);
 
 
- 
+    effaceChampInput("citation");
+    effaceChampInput("nomAuteur");
+})
+
+
+
 
 
 
